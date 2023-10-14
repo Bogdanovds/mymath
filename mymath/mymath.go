613556c0f86a881568fb72321730d41719178337
dm <dmitriebo9danov@yandex.ru>
2023-10-14 19:48:28 +0300

    mymath

--- mymath/mymath.go
+++ mymath/mymath.go
@@ -1,30 +1,35 @@
 package mymath
 
-import (
-	"github.com/Bogdanovds/mymath"
-)
+import "math"
 
 func Sqrt(x float64) float64 {
-	return mymath.Sqrt(x)
+	return math.Sqrt(x)
 }
 
 func Ceil(x float64) float64 {
-	return mymath.Ceil(x)
+	return math.Ceil(x)
 }
 
 func Floor(x float64) float64 {
-	return mymath.Floor(x)
+	return math.Floor(x)
 }
 
 func Pow(x float64, y float64) float64 {
-	return mymath.Pow(x, y)
+	return math.Pow(x, y)
 }
 
 func Max(x, y float64) float64 {
-	return mymath.Max(x, y)
+	return math.Max(x, y)
 }
 
 func Min(x, y float64) float64 {
-	return mymath.Min(x, y)
+	return math.Min(x, y)
 }
 
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Yn(n int, y float64) float64 {
+	return math.Yn(n, y)
+}
